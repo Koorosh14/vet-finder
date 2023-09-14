0e940c66617d6a1c18caf1b220209b97c29513e4
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2023-09-14 20:28:00 +0330

    add vets and reviews table migrations

---
+++ database/migrations/2023_09_14_164502_create_vets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('vets', function (Blueprint $table)
+		{
+			$table->id();
+			$table->foreignId('user_id')->constrained();
+			$table->string('name');
+			$table->longText('description')->nullable();
+			$table->string('photo')->nullable();
+			$table->string('latitude')->nullable();
+			$table->string('longitude')->nullable();
+			$table->string('address')->nullable();
+			$table->string('work_hours')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('vets');
+	}
+};
