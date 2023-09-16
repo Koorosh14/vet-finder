19e9832995f7377d5e3f50d14ab7a8b14bef161a
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2023-09-16 17:12:56 +0330

    show vets from database in index

--- app/Http/Controllers/VetController.php
+++ app/Http/Controllers/VetController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vet;
 use Illuminate\Http\Request;
 
 class VetController extends Controller
 {
 	public function index()
 	{
-		return view('index');
+		return view('index', [
+			'vets' => Vet::all(),
+		]);
 	}
 
 	public function show()
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,71 +22,20 @@
 		<div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
 	</div>
 
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
+	@unless (count($vets) === 0)
+		@foreach ($vets as $vet)
+			<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center">
+				<div class="my-3 p-3">
+					<h2 class="display-5">{{$vet->name}}</h2>
+					<p class="lead">{{$vet->description}}</p>
+				</div>
 			</div>
-			<div class="bg-body-tertiary shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+		@endforeach
+	@else
+		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center">
 			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
+				<p class="lead">No vets found!</p>
 			</div>
-			<div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
 		</div>
-	</div>
-
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-		<div class="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div class="bg-body-tertiary shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-	</div>
-
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-	</div>
-
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-		<div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-		</div>
-	</div>
+	@endunless
 </x-layout>
